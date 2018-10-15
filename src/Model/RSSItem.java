895d89f64c01e1fcbf3d6d9f552e6ad241e2aa13
kamenmi1 <miloslav.kamenik@outlook.cz>
2018-10-15 15:31:05 +0200

    Pokračujeme dalšími kody + TODO kody

---
+++ src/Model/RSSItem.java
@@ -0,0 +1,50 @@
+package Model;
+
+public class RSSItem {
+    private String title;
+    private String descrition;
+    private String link;
+    private String pubDate;
+    private String author;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescrition() {
+        return descrition;
+    }
+
+    public void setDescrition(String descrition) {
+        this.descrition = descrition;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getPubDate() {
+        return pubDate;
+    }
+
+    public void setPubDate(String pubDate) {
+        this.pubDate = pubDate;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+}
--- src/cz/uhk/fim/rssfeeder/gui/MainFrame.java
+++ src/cz/uhk/fim/rssfeeder/gui/MainFrame.java
@@ -1,15 +1,46 @@
 package cz.uhk.fim.rssfeeder.gui;
 
-import javax.swing.*;
+import utils.FileUtils;
 
-public class MainFrame extends JFrame{
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+
+public class MainFrame extends JFrame {
 
     public void init() {
         setTitle("RSSfeeder");
-        setSize(500,800);
+        setSize(500, 800);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+
+        initContentUI();
     }
+
+    private void initContentUI() {
+        JPanel controlPanel = new JPanel(new BorderLayout());
+
+        JButton btnLoad = new JButton("Load");
+        JTextField txtInputField = new JTextField();
+        JButton btnSave = new JButton("save");
+
+        controlPanel.add(btnLoad, BorderLayout.WEST);
+        controlPanel.add(txtInputField, BorderLayout.CENTER);
+        controlPanel.add(btnSave, BorderLayout.EAST);
+
+        add(controlPanel,BorderLayout.NORTH);
+
+        JTextArea txtContent = new JTextArea();
+        add(new JScrollPane(txtContent), BorderLayout.CENTER);
+
+        try {
+            txtContent.setText(FileUtils.loadStringFromFile("rss.xml"));
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
     public MainFrame() {
         init();
     }
