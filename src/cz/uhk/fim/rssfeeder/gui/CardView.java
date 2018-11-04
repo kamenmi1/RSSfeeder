c6f4f3e484c4b1293fb78edea79ae93dab29479b
kamenmi1 <miloslav.kamenik@outlook.cz>
2018-11-04 13:46:08 +0100

    Projekt RSS Nefunguje!

--- src/cz/uhk/fim/rssfeeder/gui/CardView.java
+++ src/cz/uhk/fim/rssfeeder/gui/CardView.java
@@ -4,6 +4,7 @@ import Model.RSSItem;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class CardView extends JPanel {
@@ -11,6 +12,15 @@ public class CardView extends JPanel {
     private static final int ITEM_WIDTH = 180;
     private static final int COMPONENT_WIDTH = 160;
     private static final int HEIGHT = 1;
+
+    private int r,g,b = 0;
+    private ArrayList<Color> nahodnaBarva = new ArrayList<>();
+    private ArrayList<Color> Color = new ArrayList<>();
+    private final ArrayList<Integer> finalniBarvaR = new ArrayList<>();
+    private final ArrayList<Integer> finalniBarvaG = new ArrayList<>();
+    private final ArrayList<Integer> finalniBarvaB = new ArrayList<>();
+
+    private Color barvaPozadi;
 
 
     final String startHtml = "<html><p style='width:" + COMPONENT_WIDTH + " px'>";
@@ -21,8 +31,40 @@ public class CardView extends JPanel {
         setSize(ITEM_WIDTH, HEIGHT);
         setTitle(item.getTitle());
         setDescription(item.getDescrition());
-        setBackground(setRandomColor());
         setInfo(String.format("%s - %s", item.getPubDate(), item.getAuthor()));
+        createRandomColor();
+        nastavBarvu();
+        //System.out.println(nahodnaBarva);
+        //System.out.println("===============");
+        System.out.println(barvaPozadi);
+        System.out.println("=========");
+        setBackground(new Color(r,g,b));
+    }
+
+    private void nastavBarvu() {
+        //ArrayList<java.awt.Color> color = randomColor;
+        //Color = color;
+        finalniBarvaR.add(0,214);
+        finalniBarvaG.add(0,161);
+        finalniBarvaB.add(0,147);
+        finalniBarvaR.add(1,141);
+        finalniBarvaG.add(1,142);
+        finalniBarvaB.add(1,163);
+        finalniBarvaR.add(2,201);
+        finalniBarvaG.add(2,142);
+        finalniBarvaB.add(2,163);
+
+
+        for (int i = 0; i < finalniBarvaR.size(); i++) {
+            r = finalniBarvaR.get(i);
+        }
+        for (int i = 0; i < finalniBarvaG.size(); i++) {
+            g = finalniBarvaG.get(i);
+        }
+        for (int i = 0; i < finalniBarvaB.size(); i++) {
+            b = finalniBarvaB.get(i);
+        }
+
     }
 
     private void setInfo(String format) {
@@ -37,7 +79,7 @@ public class CardView extends JPanel {
         JLabel lblDestription = new JLabel();
         lblDestription.setSize(COMPONENT_WIDTH, HEIGHT);
         lblDestription.setFont(new Font("Courier New", Font.PLAIN, 11)); // Zdroje: (http://www.java2s.com/Tutorial/Java/0240__Swing/SetFontandforegroundcolorforaJLabel.htm)
-        lblDestription.setText(String.format("%s%s%s", startHtml, descrition, endHtml));
+        lblDestription.setText(String.format("%s%5.35s%s%s", startHtml, descrition, "...", endHtml));
         add(lblDestription);
     }
 
@@ -45,11 +87,11 @@ public class CardView extends JPanel {
         JLabel lblTitle = new JLabel();
         lblTitle.setSize(COMPONENT_WIDTH, HEIGHT);
         lblTitle.setFont(new Font("Courier New", Font.BOLD, 12)); // Zdroje: (http://www.java2s.com/Tutorial/Java/0240__Swing/SetFontandforegroundcolorforaJLabel.htm)
-        lblTitle.setText(String.format("%s%5.35s%s%s", startHtml, title, "...", endHtml));
+        lblTitle.setText(String.format("%s%s%s", startHtml, title, endHtml));
         add(lblTitle);
     }
 
-    public Color setRandomColor() {
+    public void createRandomColor() {
         // Zdroje: https://stackoverflow.com/questions/4246351/creating-random-colour-in-java
         // Java 'Color' class takes 3 floats, from 0 to 1.
         Random rand = new Random();
@@ -57,7 +99,12 @@ public class CardView extends JPanel {
         float g = (float) (rand.nextFloat() / 2f + 0.5);
         float b = (float) (rand.nextFloat() / 2f + 0.5);
         Color randomColor = new Color(r, g, b);
-        return randomColor;
+        nahodnaBarva.add(randomColor);
+
+        for (int i = 0; i < nahodnaBarva.size(); i++) {
+            Color.add(nahodnaBarva.get(i));
+            System.out.println(Color);
+        }
     }
 }
 
